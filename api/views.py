c3052ec241a88419f5e6080bc272487655e4ee2c
saleh dadgar <salehdadgar220@gmail.com>
2021-07-22 15:19:52 +0430

    viewset

--- api/views.py
+++ api/views.py
@@ -4,22 +4,27 @@ from django.shortcuts import render
 from django_rest.models import Articel
 from .permissions import IsSuperUser, IsStaffOrReadOnly, IsAuthorOrReadOnly, IsSuperUserOrStaffReadOnly
 from .serializers import ArticelSerializers, UserSerializer
-from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, DestroyAPIView
+from rest_framework.generics import ListCreateAPIView,
+from rest_framework.viewsets import ModelViewSet
 
 
 # Create your views here.
 
-class Articel_list(ListCreateAPIView, DestroyAPIView):
-    queryset = Articel.objects.all()
+# class Articel_list(ListCreateAPIView, DestroyAPIView):
+#     queryset = Articel.objects.all()
+#     serializer_class = ArticelSerializers
+#
+#
+# class ArticelDetail(RetrieveAPIView, DestroyAPIView):
+#     queryset = Articel.objects.all()
+#     serializer_class = ArticelSerializers
+#     permission_classes = (IsAuthorOrReadOnly, IsStaffOrReadOnly)
+
+class ArticelSet(ModelViewSet):
+    queryset =  Articel.objects.all()
     serializer_class = ArticelSerializers
 
-
-class ArticelDetail(RetrieveAPIView, DestroyAPIView):
-    queryset = Articel.objects.all()
-    serializer_class = ArticelSerializers
-    permission_classes = (IsAuthorOrReadOnly, IsStaffOrReadOnly)
-
-
+    
 class UserList(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
--- djangorest/urls.py
+++ djangorest/urls.py
@@ -13,21 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from dj_rest_auth.views import PasswordResetConfirmView
+from dj_rest_auth.views import PasswordResetConfirmView
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt import views as jwt_views
+# from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('django_rest.urls')),
     path('api/', include('api.urls')),
 
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 
-    # path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    # path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    # path('password/reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('api/rest-auth/', include('dj_rest_auth.urls')),
+    path('api/rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/rest-auth/password/reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 
 ]
 
